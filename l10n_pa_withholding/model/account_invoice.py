2cdaa9d609536cad2c3c4092d631e2aa70283ded
Humberto Arocha <hbto@vauxoo.com>
2016-10-26 06:25:46 +0000

    [FIX] First elements in selection field were set as strings

--- l10n_pa_withholding/model/account_invoice.py
+++ l10n_pa_withholding/model/account_invoice.py
@@ -17,14 +17,14 @@ class AccountInvoice(models.Model):
     # /!\ NOTE: This code will be regarded as duplicated
     l10n_pa_wh_subject = fields.Selection([
         ('na', 'No Aplica'),
-        (1, 'Pago por Servicio Profesional al Estado 100%'),
-        (2, 'Pago por Venta de Bienes/Servicios al Estado 50%'),
-        (3, 'Pago o Acreditacion a No Domiciliado o Empresa Constituida en el'
-         ' Exterior 100%'),
-        (4, 'Pago o Acreditacion por Compra de Bienes/Servicios 50%'),
-        (5, 'Pago a Comercio Afiliado a Sistema de TC/TD 2%'),
-        (6, 'Pago a Comercio Afiliado a Sistema de TC/TD 1%'),
-        (7, 'Pago a Comercio Afiliado a Sistema de TC/TD 50%')],
+        ('1', 'Pago por Servicio Profesional al Estado 100%'),
+        ('2', 'Pago por Venta de Bienes/Servicios al Estado 50%'),
+        ('3', 'Pago o Acreditacion a No Domiciliado o Empresa Constituida en'
+         ' el Exterior 100%'),
+        ('4', 'Pago o Acreditacion por Compra de Bienes/Servicios 50%'),
+        ('5', 'Pago a Comercio Afiliado a Sistema de TC/TD 2%'),
+        ('6', 'Pago a Comercio Afiliado a Sistema de TC/TD 1%'),
+        ('7', 'Pago a Comercio Afiliado a Sistema de TC/TD 50%')],
         string='ITBMS Withholding Subject',
         help='If Apply. Indicates how much ITBMS to withholding on Payment')
 
