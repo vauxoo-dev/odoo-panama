3e671aac8fdb0f27d69870b2681d4f131b4e95a5
Humberto Arocha <hbto@vauxoo.com>
2016-10-27 23:22:19 +0000

    [IMP] Taking away condition from the code

--- l10n_pa_withholding/model/stock.py
+++ l10n_pa_withholding/model/stock.py
@@ -11,7 +11,5 @@ class StockPicking(models.Model):
         res = super(StockPicking, self)._get_invoice_vals(
             key, inv_type, journal_id, move)
         order = move.picking_id.sale_id
-        if not order:
-            return res
-        return dict(res, wh_agent_itbms=order.wh_agent_itbms,
-                    l10n_pa_wh_subject=order.l10n_pa_wh_subject)
+        return dict(res, wh_agent_itbms=order and order.wh_agent_itbms,
+                    l10n_pa_wh_subject=order and order.l10n_pa_wh_subject)
